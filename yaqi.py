c4eb4e98c0358a1b586d0a71c20488c16f0ecc5f
payambarneanderthal <99961787+payambarneanderthal@users.noreply.github.com>
2022-02-23 10:17:16 +0330

    Update yaqi.py

--- yaqi.py
+++ yaqi.py
@@ -1,18 +1,12 @@
 # SECURITY 
 import requests
-
 import time
 
 target = input("enter number >>> ")
-
 url = "https://web.rubika.ir"
 
 While True:
-
 time.sleep(0.3)
-
 pyload ={"cellphone": target}
-
 u = requests.post(url, data=pyload)
-
 print(ok)
